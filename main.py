a04c5f30c9f70d2d2185e08a79290f11dda51d90
ppabis <piotr@pabis.eu>
2025-05-29 17:22:47 +0200

    Add option to print the keys

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ from write import save_all_otps
 def main():
     parser = argparse.ArgumentParser(description='Read QR codes from an image file')
     parser.add_argument('--input', required=True, help='Input image file containing QR code')
-    parser.add_argument('--output', required=True, help='Output directory to save results')
+    parser.add_argument('--output', required=False, help='Output directory to save results')
+    parser.add_argument('--print', action='store_true', help='Print OTPs to console')
     
     args = parser.parse_args()
-    
-    # Create output directory if it doesn't exist
-    os.makedirs(args.output, exist_ok=True)
+
+    if not args.output and not args.print:
+        print("Please provide either --output or --print")
+        return
     
     # Read QR code from input file
     decoded_urls = read_qr(args.input)
@@ -23,7 +25,12 @@ def main():
         payload = get_payload_from_otp_url(url, 0, "unknown")
         otps = to_otp(payload)
         otps = fix_issuers(otps)
-        save_all_otps(otps, args.output)
+        if args.output:
+            os.makedirs(args.output, exist_ok=True)
+            save_all_otps(otps, args.output)
+        elif args.print:
+            from pprint import pprint
+            pprint(otps)
 
 if __name__ == "__main__":
     main()
